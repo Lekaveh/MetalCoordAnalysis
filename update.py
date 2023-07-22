33ff0daab56cd8efd184bd9cf851e4b276ae8042
Kaveh <Lekaveh@gmail.com>
2023-07-22 17:32:53 +0400

    New functionality added: update distances in cif file. main.py deleted and changed by statspy

--- update.py
+++ update.py
@@ -5,6 +5,8 @@ import os
 import gemmi
 import os
 from pathlib import Path
+import argparse
+import json
 
 
 def decompose(values, n):
@@ -119,3 +121,70 @@ def differ(path):
         print(path)
     
     return [name, None, None]
+
+
+def contains_metal(block):
+    loop = block.find_loop ('_chem_comp_atom.comp_id').get_loop()
+    rows = decompose(loop.values, len(loop.tags))
+    for row in rows:
+        if gemmi.Element(row[2]).is_metal:
+            return True
+    return False
+
+def get_element_name_dict(block):
+    result = dict()
+    loop = block.find_loop ('_chem_comp_atom.comp_id').get_loop()
+    rows = decompose(loop.values, len(loop.tags))
+    for row in rows:
+        result[row[1]] =  row[2]
+    return result
+
+def adjust(output_path, path):
+    try:
+        folder, name = os.path.split(path)
+        name = name[:-4]
+        folder = os.path.split(folder)[1]
+        doc = cif.read_file(path)
+        block = doc.find_block(f"comp_{name}")
+        loop = block.find_loop ('_chem_comp_bond.value_dist').get_loop()
+        rows = decompose(loop.values, len(loop.tags))
+        if name in mons and contains_metal(block):
+            el_name = get_element_name_dict(block)
+            pdb = mons[name][0][0]
+            results = find_classes(name, pdb)
+            for row in rows:
+                
+                metal_name = row[1]
+                ligand_name = row[2]
+                if not gemmi.Element(el_name[row[2]]).is_metal and not gemmi.Element(el_name[row[1]]).is_metal:
+                    continue
+
+                if gemmi.Element(row[2]).is_metal:
+                    metal_name = row[2]
+                    ligand_name = row[1]
+                distance, std, cl, procrustes, coordination = get_stats(results, metal_name, ligand_name)
+                if coordination > 0:
+                    row[4] = row[6] = str(round(distance, 3))
+                    row[5] = row[7] = str(round(std, 3))
+            
+            loop.set_all_values(pack(rows))
+            Path(os.path.split(output_path)[0]).mkdir(exist_ok=True, parents=True)
+            doc.write_file(output_path)
+    except Exception as e:
+        print(e)
+        print(path)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--ligand', required=True, help='ligand path')
+    parser.add_argument('--output', required=True, help='output file path')
+
+    args = parser.parse_args()
+
+    ligand = args.ligand
+    output = args.output
+
+
+    adjust(output, ligand)
+    
