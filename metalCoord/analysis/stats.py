b0eb8fcda38eff2c391ae15aa6704255d8ed05d1
Kave <lekaveh@gmail.com>
2023-11-20 21:05:07 +0400

    CIF processing added.

--- metalCoord/analysis/stats.py
+++ metalCoord/analysis/stats.py
@@ -1,10 +1,10 @@
 
 import numpy as np
 from tqdm import tqdm
-
 from metalCoord.load.rcsb import load_pdb
 from metalCoord.analysis.structures import get_ligands
 import gemmi
+import os
 from metalCoord.analysis.data import DB
 from metalCoord.analysis.data import StrictCandidateFinder, ElementCandidateFinder, ElementInCandidateFinder, AnyElementCandidateFinder, NoCoordinationCandidateFinder
 from metalCoord.analysis.data import StrictCorrespondenceStatsFinder, WeekCorrespondenceStatsFinder, OnlyDistanceStatsFinder
@@ -42,17 +42,25 @@ def generateJson(stats):
 
 
 
-def get_structures(ligand, pdb_name):
-    if pdb_name.endswith(".pdb"):
-        st = gemmi.read_pdb(pdb_name)   
-    else:    
-        pdb, type = load_pdb(pdb_name)
+def get_structures(ligand, path):
+  
+    if os.path.isfile(path):
+        st = gemmi.read_structure(path)
+        return get_ligands(st, ligand)
+
+    elif len(path) == 4:         
+        pdb, type = load_pdb(path)
         if type == 'cif':
-            Logger().error("Unsupported data format cif")
-        st = gemmi.read_pdb_string(pdb)
+            cif_block = gemmi.cif.read_string()[0]
+            st = gemmi.make_structure_from_block(cif_block)
+        else:
+            st = gemmi.read_pdb_string(pdb)
+        return get_ligands(st, ligand)
 
-    return get_ligands(st, ligand)
+    else:
+        raise Exception("Existing pdb or mmcif file path should be provided or 4 letter pdb code")
 
+    
 
 
 strategies = [StrictCorrespondenceStatsFinder(StrictCandidateFinder()),
