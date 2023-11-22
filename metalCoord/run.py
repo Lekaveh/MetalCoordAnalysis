9cb6eb1fa848053d734b4164244dd9c66b3eaeb9
Kave <lekaveh@gmail.com>
2023-11-22 21:17:33 +0400

    Version added. Help updated.

--- metalCoord/run.py
+++ metalCoord/run.py
@@ -207,24 +207,24 @@ def adjust(output_path, path, pdb):
     except Exception as e:
         Logger().error(f"Error in {path}. {e}")
 
-
 def create_parser():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(prog='metalCoord', description='MetalCoord: Metal coordination analysis.')
+    parser.add_argument('--version', action='version', version='%(prog)s 0.1.1')
 
-    # Define the subparsers for the two apps
+    # Define the subparsers for the two apps    
     subparsers = parser.add_subparsers(dest='command')
 
     # App1
     update_parser = subparsers.add_parser('update', help='Update a cif file.')
-    update_parser.add_argument('-i', '--input', type=str, required=True, help='Path to the input CIF file.')
-    update_parser.add_argument('-o', '--output', type=str, required=True, help='Updated CIF file path.')
-    update_parser.add_argument('-p', '--pdb', type=str, required=False, help='PDB name or path to the pdb.')
+    update_parser.add_argument('-i', '--input', type=str, required=True, help='CIF file.', metavar='<INPUT CIF FILE>')
+    update_parser.add_argument('-o', '--output', type=str, required=True, help='Output cif file.', metavar='<OUTPUT CIF FILE>')
+    update_parser.add_argument('-p', '--pdb', type=str, required=False, help='PDB code or pdb file.', metavar='<PDB CODE|PDB FILE>')
 
     # App2
-    stats_parser = subparsers.add_parser('stats', help='Distance statistics.')
-    stats_parser.add_argument('-l', '--ligand', type=str, required=True, help='Ligand namr.')
-    stats_parser.add_argument('-p', '--pdb', type=str, required=True, help='PDB name or path to the pdb.')
-    stats_parser.add_argument('-o', '--output', type=str, required=True, help='Output file path.')
+    stats_parser = subparsers.add_parser('stats', help='Distance and angle statistics.')
+    stats_parser.add_argument('-l', '--ligand', type=str, required=True, help='Ligand code.', metavar='<LIGAND CODE>')
+    stats_parser.add_argument('-p', '--pdb', type=str, required=True, help='PDB code or pdb file.', metavar='<PDB CODE|PDB FILE>')
+    stats_parser.add_argument('-o', '--output', type=str, required=True, help='Output json file.', metavar='<OUTPUT JSON FILE>')
 
     return parser
 
@@ -234,6 +234,7 @@ def main_func():
     try:
         parser = create_parser()
         args = parser.parse_args()
+        parser.print_help()
 
         if args.command == 'update':
             adjust(args.output, args.input, args.pdb)
