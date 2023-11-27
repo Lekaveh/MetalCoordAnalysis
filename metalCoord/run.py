5732255c60c409a0fd339415bb49d5f65f5f4515
Kave <lekaveh@gmail.com>
2023-11-27 22:31:32 +0400

    Version updated to 0.1.3 Now the program works with ligands got from the PDB (www.rcsb.org)

--- metalCoord/run.py
+++ metalCoord/run.py
@@ -11,6 +11,22 @@ import json
 
 
 
+_angle_category = "_chem_comp_angle"
+_bond_category = "_chem_comp_bond"
+_atom_category = "_chem_comp_atom"
+_comp_id = "comp_id"
+_atom_id = "atom_id"
+_type_symbol = "type_symbol"
+_atom_id_1 = "atom_id_1"
+_atom_id_2 = "atom_id_2"
+_atom_id_3 = "atom_id_3"
+_value_dist_nucleus = "value_dist_nucleus"
+_value_dist_nucleus_esd = "value_dist_nucleus_esd"
+_value_dist = "value_dist"
+_value_dist_esd = "value_dist_esd"
+_value_angle = "value_angle"
+_value_angle_esd = "value_angle_esd"
+
 def decompose(values, n):
     result = []
     for i in range(0, len(values)//n):
@@ -23,103 +39,90 @@ d = os.path.dirname(sys.modules["metalCoord"].__file__)
 mons = json.load(open(os.path.join(d, "data/mons.json")))
 
 
-def get_distance(results, metal_name, ligand_name):
+def get_distance(clazz, ligand_name):
     coordination = 0
     cl = ""
     procrustes = 1
     distance = -1
     std = -1
 
-    for metal in results:
-        if metal["metal"] == metal_name:
-            for clazz in metal["ligands"]:
-                if clazz["coordination"] < coordination:
-                        continue
-                for ligand in clazz["base"]:
-                    if ligand["ligand"]["name"]  == ligand_name:
-                        if clazz["coordination"] == coordination and  float(clazz["procrustes"]) < procrustes:
-                            coordination =  clazz["coordination"]
-                            procrustes   =  float(clazz["procrustes"])
-                            distance = ligand["distance"]
-                            std = ligand["std"]
-                            cl = clazz["class"]
 
-                        if clazz["coordination"] > coordination:
-                            coordination =  clazz["coordination"]
-                            procrustes   =  float(clazz["procrustes"])
-                            distance = ligand["distance"]
-                            std = ligand["std"]
-                            cl = clazz["class"]
+    if clazz is None:
+        return distance, std, cl, procrustes, coordination
+
+    for ligand in clazz["base"]:
+        if ligand["ligand"]["name"]  == ligand_name:
+            coordination =  clazz["coordination"]
+            procrustes   =  float(clazz["procrustes"])
+            distance = ligand["distance"]
+            std = ligand["std"]
+            cl = clazz["class"]
+            break
                         
-                        break
     return distance, std, cl, procrustes, coordination
 
-def get_angles(results, metal_name, ligand_name1, ligand_name2):
+def get_angles(clazz, ligand_name1, ligand_name2):
     coordination = 0
     cl = ""
     procrustes = 1
     angle = -1
     std = -1
 
+    if clazz is None:
+        return angle, std, cl, procrustes, coordination
+
+    for ligand in clazz["angles"]:
+        if ((ligand["ligand1"]["name"] == ligand_name1) and (ligand["ligand2"]["name"]  == ligand_name2)) or ((ligand["ligand2"]["name"]  == ligand_name1) and (ligand["ligand1"]["name"]  == ligand_name2)):
+            coordination =  clazz["coordination"]
+            procrustes   =  float(clazz["procrustes"])
+            angle = ligand["angle"]
+            std = ligand["std"]
+            cl = clazz["class"]
+            break
+                        
+                        
+    return angle, std, cl, procrustes, coordination
+
+def get_best(results, metal_name):
+    coordination = 0
+    procrustes = 1
+
+    result = None
     for metal in results:
         if metal["metal"] == metal_name:
             for clazz in metal["ligands"]:
                 if clazz["coordination"] < coordination:
                         continue
-                for ligand in clazz["angles"]:
-                    if ((ligand["ligand1"]["name"] == ligand_name1) and (ligand["ligand2"]["name"]  == ligand_name2)) or ((ligand["ligand2"]["name"]  == ligand_name1) and (ligand["ligand1"]["name"]  == ligand_name2)):
-                        if clazz["coordination"] == coordination and  float(clazz["procrustes"]) < procrustes:
-                            coordination =  clazz["coordination"]
-                            procrustes   =  float(clazz["procrustes"])
-                            angle = ligand["angle"]
-                            std = ligand["std"]
-                            cl = clazz["class"]
 
-                        if clazz["coordination"] > coordination:
-                            coordination =  clazz["coordination"]
-                            procrustes   =  float(clazz["procrustes"])
-                            angle = ligand["angle"]
-                            std = ligand["std"]
-                            cl = clazz["class"]
-                        
-                        break
-    return angle, std, cl, procrustes, coordination
+                if clazz["coordination"] == coordination and  float(clazz["procrustes"]) < procrustes:
+                    coordination =  clazz["coordination"]
+                    procrustes   =  float(clazz["procrustes"])
+                    result = clazz
 
+                if clazz["coordination"] > coordination:
+                    coordination =  clazz["coordination"]
+                    procrustes   =  float(clazz["procrustes"])
+                    result = clazz
 
-def differ(path):
-    try:
-        folder, name = os.path.split(path)
-        name = name[:-4]
-        folder = os.path.split(folder)[1]
-        doc = cif.read_file(path)
-        block = doc.find_block(f"comp_{name}")
-        if name in mons:
-            el_name = get_element_name_dict(block)
-            pdb = mons[name][0][0]
-            structures = get_structures(name, pdb)
-            return [name, len(structures) > 0, contains_metal(block)]
-    except Exception as e:
-        print(e)
-        print(path)
-    
-    return [name, None, None]
+    if result is None:
+        Logger().info(f"No class for {metal_name} found. Please check the pdb file")
+    else:
+        Logger().info(f"Best class for {metal_name} is {result['class']} with coordination {coordination} and procrustes {procrustes}")
+    return result
 
+   
 
-def contains_metal(block):
-    loop = block.find_loop ('_chem_comp_atom.comp_id').get_loop()
-    rows = decompose(loop.values, len(loop.tags))
-    for row in rows:
-        if gemmi.Element(row[2]).is_metal:
+def get_element_name(mmcif_atom_category, name):
+    for i, _ in enumerate(mmcif_atom_category[_atom_id]):
+        if _ == name:
+            return mmcif_atom_category[_type_symbol][i]
+
+def contains_metal(mmcif_atom_category):
+
+    for element in mmcif_atom_category[_type_symbol]:
+        if gemmi.Element(element).is_metal:
             return True
     return False
-
-def get_element_name_dict(block):
-    result = dict()
-    loop = block.find_loop ('_chem_comp_atom.comp_id').get_loop()
-    rows = decompose(loop.values, len(loop.tags))
-    for row in rows:
-        result[row[1]] =  row[2]
-    return result
 
 
 def adjust(output_path, path, pdb):
@@ -128,13 +131,28 @@ def adjust(output_path, path, pdb):
     name = name[:-4]
     folder = os.path.split(folder)[1]
     doc = cif.read_file(path)
-    block = doc.find_block(f"comp_{name}")
+    block = doc.find_block(f"comp_{name}") if doc.find_block(f"comp_{name}") is not None else doc.find_block(f"{name}")
+
+
+    if block is None:
+        Logger().error(f"No block found for {name}|comp_{name}. Please check the cif file.")
+        return
     
+    atoms = block.get_mmcif_category(_atom_category)
+    bonds = block.get_mmcif_category(_bond_category)
+    angles = block.get_mmcif_category(_angle_category)
     
-    if name in mons and contains_metal(block):
-        loop = block.find_loop ('_chem_comp_bond.value_dist').get_loop()
-        rows = decompose(loop.values, len(loop.tags))
-        el_name = get_element_name_dict(block)
+    if not atoms:
+        Logger().error(f"mmcif category {_atom_category} not found. Please check the cif file.")
+        return
+    
+    if not bonds:
+        Logger().error(f"mmcif category {_bond_category} not found. Please check the cif file.")
+        return
+    
+    if name in mons and contains_metal(atoms):
+
+
         if pdb is None:
             Logger().info(f"Choosing best pdb file")
             pdb = mons[name][0][0]
@@ -142,52 +160,83 @@ def adjust(output_path, path, pdb):
 
         results = find_classes(name, pdb)
 
+
         if len(results) == 0:
             Logger().info(f"No {name} found in {pdb}. Please check the pdb file")
             return
         
+
+     
+
+
+        best_results = dict()
+        for atom_name, element in  zip(atoms[_atom_id], atoms[_type_symbol]):
+            if gemmi.Element(element).is_metal:
+                best_results[atom_name] = get_best(results, atom_name)
+
+        
         Logger().info(f"Ligand updating started")
-        for row in rows:
-            
-            metal_name = row[1]
-            ligand_name = row[2]
-            if not gemmi.Element(el_name[row[2]]).is_metal and not gemmi.Element(el_name[row[1]]).is_metal:
+        if _value_dist not in bonds:
+            bonds[_value_dist] = ["0.00"] * len(bonds[_atom_id_1])
+            bonds[_value_dist_esd] = ["0.00"] * len(bonds[_atom_id_1])
+            bonds[_value_dist_nucleus] = ["0.00"] * len(bonds[_atom_id_1])
+            bonds[_value_dist_nucleus_esd] = ["0.00"] * len(bonds[_atom_id_1])
+
+        for i, _atoms in enumerate(zip(bonds[_atom_id_1], bonds[_atom_id_2])):
+            metal_name, ligand_name = _atoms
+
+            if not gemmi.Element(get_element_name(atoms, metal_name)).is_metal and not gemmi.Element(get_element_name(atoms, ligand_name)).is_metal:
                 continue
 
-            if gemmi.Element(row[2]).is_metal:
-                metal_name = row[2]
-                ligand_name = row[1]
+            if gemmi.Element(get_element_name(atoms, ligand_name)).is_metal:
+                metal_name, ligand_name = ligand_name, metal_name
 
 
-            distance, std, cl, procrustes, coordination = get_distance(results, metal_name, ligand_name)
+            distance, std, _, _, coordination = get_distance(best_results.get(metal_name, None), ligand_name)
             if coordination > 0:
-                row[4] = row[6] = str(round(distance, 3))
-                row[5] = row[7] = str(round(std, 3))
+                bonds[_value_dist][i] = bonds[_value_dist_nucleus][i] = str(round(distance, 3))
+                bonds[_value_dist_esd][i] = bonds[_value_dist_nucleus_esd][i] = str(round(std, 3))
             
-        
-        loop.set_all_values(pack(rows))
+
+        block.set_mmcif_category(_bond_category, bonds)
         Logger().info(f"Distances updated")
 
 
+        if not angles:
+            Logger().info(f"Creating mmcif category {_angle_category}.")
+            angles[_comp_id] = list()
+            angles[_atom_id_1] = list()
+            angles[_atom_id_2] = list()
+            angles[_atom_id_3] = list()
+            angles[_value_angle] = list()
+            angles[_value_angle_esd] = list()
 
-        loop = block.find_loop ('_chem_comp_angle.value_angle').get_loop()
-        rows = decompose(loop.values, len(loop.tags))
-        if name in mons and contains_metal(block):
-            el_name = get_element_name_dict(block)
-            for row in rows:
-                
-                ligand1_name = row[1]
-                metal_name = row[2]
-                ligand2_name = row[3]
-                if not gemmi.Element(el_name[row[2]]).is_metal:
+            for metal_name, clazz in best_results.items():
+                if clazz is None:
+                    continue
+                for ligand in clazz["angles"]:
+                    if ligand["ligand1"]["residue"] == name and ligand["ligand2"]["residue"] == name:
+                        angles[_comp_id].append(name)
+                        angles[_atom_id_1].append(ligand["ligand1"]["name"])
+                        angles[_atom_id_2].append(metal_name)
+                        angles[_atom_id_3].append(ligand["ligand2"]["name"])
+                        angles[_value_angle].append(str(round(ligand["angle"], 3)))
+                        angles[_value_angle_esd].append(str(round(ligand["std"], 3)))
+
+        else:
+            for i, _atoms in enumerate(zip(angles[_atom_id_1], angles[_atom_id_2], angles[_atom_id_3])):
+                ligand1_name, metal_name, ligand2_name = _atoms
+
+                if not gemmi.Element(get_element_name(atoms, metal_name)).is_metal:
                     continue
 
-                angle, std, cl, procrustes, coordination = get_angles(results, metal_name, ligand1_name, ligand2_name)
+                angle, std, _, _, coordination = get_angles(best_results.get(metal_name, None), ligand1_name, ligand2_name)
                 if coordination > 0:
-                    row[4]  = str(round(angle, 3))
-                    row[5]  = str(round(std, 3))
+                    angles[_value_angle][i] = str(round(angle, 3))
+                    angles[_value_angle_esd][i] = str(round(std, 3))
             
-        loop.set_all_values(pack(rows))
+            
+        block.set_mmcif_category(_angle_category, angles)
         Logger().info(f"Angles updated")
         Logger().info(f"Ligand update completed")
 
@@ -209,7 +258,7 @@ def adjust(output_path, path, pdb):
 
 def create_parser():
     parser = argparse.ArgumentParser(prog='metalCoord', description='MetalCoord: Metal coordination analysis.')
-    parser.add_argument('--version', action='version', version='%(prog)s 0.1.2')
+    parser.add_argument('--version', action='version', version='%(prog)s 0.1.3')
 
     # Define the subparsers for the two apps    
     subparsers = parser.add_subparsers(dest='command')
@@ -249,7 +298,7 @@ def main_func():
         else:
             parser.print_help()
     except Exception as e:
-        Logger().error(f"Error: {e}")
+        Logger().error(f"{repr(e)}")
 
 if __name__ == '__main__':
     main_func()    
