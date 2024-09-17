daf41b821534d604fc462a94e3921f04724e9f4e
Kave <lekaveh@gmail.com>
2024-09-17 20:32:27 +0400

    Prosmatic changed to prismatic

--- metalCoord/__init__.py
+++ metalCoord/__init__.py
@@ -1,5 +1,5 @@
 from metalCoord.services import update_cif, get_stats
 from metalCoord.analysis.classify import find_classes
 
-__version__ = "0.1.41"
+__version__ = "0.1.42"
 __all__ = ['update_cif', 'get_stats', 'find_classes', '__version__']
